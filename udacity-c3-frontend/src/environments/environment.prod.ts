ac4c6dcdf105a949b400f42de86b707067807f91
Zubair Riaz <zubairriaz78696@gmail.com>
2020-03-30 17:16:30 +0500

    set up CI/CD
    setting up CI CD on the deployments

--- udacity-c3-frontend/src/environments/environment.prod.ts
+++ udacity-c3-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae6a46882041d4ebdb4d0d1a9d7a2168-126506738.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
--- udacity-c3-frontend/src/environments/environment.ts
+++ udacity-c3-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udagram-api-lb-1178005182.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
